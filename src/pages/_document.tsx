6938e560bac3e184337c459a8c3d912fc9467b16
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 17:34:36 -0400

    add link for font Poppins

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,16 @@ export default function Document() {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta name="description" content="Site simples com Next.js" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
