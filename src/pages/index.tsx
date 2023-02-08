358a7829b560b58b955d16dc6567e3e52ea03b54
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 17:35:26 -0400

    update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="bg-cyan-900 flex justify-center items-center flex-col min-w-full min-h-screen px-3">
+      <h1 className="font-['Poppins']">Home</h1>
+    </div>
+  );
 }
