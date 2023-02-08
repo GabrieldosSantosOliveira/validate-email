699dd7e3d53fe0530529fe1fa9024ff46bb5dd86
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 15:54:49 -0400

    remove styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,4 @@
 import type { AppProps } from 'next/app';
-import { GlobalStyles } from 'styles/global';
-export default function App({
-  Component,
-  pageProps,
-}: AppProps) {
-  return (
-    <>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
